262cb1505daa4804cd9115d046095696b7fbbb54
sahra-amir <Sahra.Amir@aogroup.co.za>
2023-04-20 17:38:39 +0200

    the accordion and footer section of the Digital page

---
+++ AccordionFooterWebPart/src/webparts/accordionFooter/components/IAccordionFooterProps.ts
@@ -0,0 +1,7 @@
+export interface IAccordionFooterProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ AccordionFooterWebPart/src/webparts/accordionFooter/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IAccordionFooterWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'AccordionFooterWebPartStrings' {
+  const strings: IAccordionFooterWebPartStrings;
+  export = strings;
+}
