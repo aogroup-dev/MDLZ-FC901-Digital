a034a1b471c89fbbb401a7d850931ec00bc55394
PaulVZ <paulvz@gmail.com>
2023-04-26 10:21:44 +0200

    remove sync

--- banner/src/webparts/digitalRnDBanner/components/DigitalRnDBanner.tsx
+++ banner/src/webparts/digitalRnDBanner/components/DigitalRnDBanner.tsx
@@ -3,7 +3,7 @@ import styles from './DigitalRnDBanner.module.scss';
 import { IDigitalRnDBannerProps } from './IDigitalRnDBannerProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 
-import { sp } from "@pnp/sp/presets/all";
+// import { sp } from "@pnp/sp/presets/all";
 import "@pnp/sp/lists";
 
 export default class DigitalRnDBanner extends React.Component<IDigitalRnDBannerProps, {}> {
@@ -14,27 +14,27 @@ export default class DigitalRnDBanner extends React.Component<IDigitalRnDBannerP
       bannerDesc,
     } = this.props;
 
-    const siteUrl =
-      "https://collaboration.mdlz.com/sites/MondelezUSteamportal/";
+    // const siteUrl =
+    //   "https://collaboration.mdlz.com/sites/MondelezUSteamportal/";
 
-    sp.setup({
-      sp: {
-        baseUrl: siteUrl,
-      },
-    });
+    // sp.setup({
+    //   sp: {
+    //     baseUrl: siteUrl,
+    //   },
+    // });
 
-    sp.web.lists
-      .getByTitle("Pages")
-      .items.select("Id", "Title", "PublishingPageContent", "PublishingPageLayout", "News_x0020_Type", "Created")
-      .orderBy("Created", false)
-      .filter("News_x0020_Type eq 'Allocations' and (Created gt '2022-01-01' and Created lt '2023-01-01')")
-      .get()
-      .then((items: any[]) => {
-        console.log(items);
-      })
-      .catch((error: any) => {
-        console.log("Error retrieving items from list:", error);
-      });
+    // sp.web.lists
+    //   .getByTitle("Pages")
+    //   .items.select("Id", "Title", "LinkFilename", "PublishingPageContent", "PublishingPageLayout", "News_x0020_Type", "Created")
+    //   .orderBy("Created", false)
+    //   .filter("News_x0020_Type eq 'Allocations' and (Created gt '2022-01-01' and Created lt '2023-01-01')")
+    //   .get()
+    //   .then((items: any[]) => {
+    //     console.log(items);
+    //   })
+    //   .catch((error: any) => {
+    //     console.log("Error retrieving items from list:", error);
+    //   });
 
     return (
       <section>
